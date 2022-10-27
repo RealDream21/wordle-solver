3621f4fac82f5af3cb4dd628ede7545621c2546f
Radu Alexandru <rd_alexandruradu10@yahoo.ro>
2022-10-27 17:54:38 +0300

    0,1,2 in functie de aparitie si pozitionare

--- proiect_asc1.py
+++ proiect_asc1.py
@@ -6,9 +6,22 @@ f = open('wordlist.txt','r')
 #luam un cuvant random
 x = random.choice(list(f))
 
+nr=input("nr=")
+#Program pentru ghcirea cuvantului
+for i in range (0,len(nr)):
+        if nr[i]==x[i]:
+            print(2)
+        else:    
+            if nr[i] in x:
+                print(1)
+            else:
+                print(0)
+
+
 #afisare
 g = open('data.out','w')
 print (x)
 g.write(x)
 f.close
 g.close
+
