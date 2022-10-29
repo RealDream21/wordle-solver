d48a8547f349523305fa1fb7f17e773fed07314f
RealDream <fabianflorete@yahoo.com>
2022-10-29 18:26:21 +0300

    Functie indice cuvinte
    Am adaugat o functie ce calculeaza indicele fiecarui cuvant in parte. Rularea dureaza vreo 3 - 4 secunde, iar cuvantul ar fi "AUIAI". De asemenea, am schimbat wordlist.txt in wordlist.in

--- main.cpp
+++ main.cpp
@@ -7,18 +7,17 @@ using namespace std;
 ifstream fin("wordlist.in");
 ofstream fout("occurance.out");
 
-
 //0 for wron
 //1 for got word right but not position
 //2 for word right, position right
 //calculate some sort of accuracy index
 
-const int N = 11454; //N = nr de cuvinte
+const int N = 11453; //N = nr de cuvinte
 char dic[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 struct wordType{
-    int cnt = 0;
-    char lit = 0;
+    float indice = 0;
+    char lit;
     char s[6];
 }word[12000];
 
@@ -27,7 +26,28 @@ struct lit{
     char lit;
 }l[30];
 
-void letter_count(wordType text)
+void indice(wordType &text)
+{
+    float sum = 0;
+    for(int r = 0; r <= N; r++)
+    {
+        char deGhicit[6];
+        strcpy(deGhicit, word[r].s);
+        for (int i = 0; i < 5; i++)
+        {
+            if(deGhicit[i] == text.s[i])
+            {
+                sum += 2;
+                deGhicit[i] = '0';
+            }
+            else if (strchr(deGhicit, text.s[i])) sum += 1;
+        }
+    }
+    text.indice = sum;
+    return ;
+}
+
+void letter_count(wordType &text)
 {
     int k = 0;
     for(int i = 0; i < strlen(text.s); i++)
@@ -40,14 +60,25 @@ void letter_count(wordType text)
         for(int j = i + 1; j <= 30; j++)
             if(l[j].ap > l[i].ap) swap(l[i], l[j]);
     cout << l[0].lit;
+    text.lit = l[0].lit;
     return;
 }
 
 int main()
 {
-    cin.getline(word[0].s, 5);
-    letter_count(word[0]);
+    for(int i = 0; i <= N; i++)
+        fin >> word[i].s;
 
-
+    int maxim = 0, imax = 0;
+    for(int i = 0; i <= N; i++)
+    {
+        indice(word[i]);
+        if (word[i].indice > maxim)
+        {
+            imax = i;
+            maxim = word[i].indice;
+        }
+    }
+    cout << word[imax].s;
     return 0;
 }
