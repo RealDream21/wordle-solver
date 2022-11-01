f126b8bf3a1063e0431987366ab9faf3b82a7a53
CybotTK <stancilaionutmarian@yahoo.com>
2022-11-01 22:59:50 +0200

    Am adaugat ideea de stergere a cuvantului din lista, pentru un cuv spec
    cuv='ROITE'
    am adaugat un wordlist.txt cu toate cuvintele
    si un data.out

--- proiect_asc1.py
+++ proiect_asc1.py
@@ -3,6 +3,7 @@ import random
 #citire si crearea listei
 f = open('wordlist.txt','r')
 
+"""
 #luam un cuvant random
 x = random.choice(list(f))
 
@@ -25,13 +26,39 @@ for i in range (0,5):
             #print(0)
            s+="0"+""
 print(s)
+"""
 
+#crearea listei
+file_content=f.read()
+content_list=file_content.split("\n")
 
+#luam un cuvant random
+x = random.choice(content_list)
+
+#sterge elementele din lista care au o literea din cuv
+cuv="ROITE"
+poz=0
+while poz < len(content_list):
+    if cuv[0] in content_list[poz]: 
+        del content_list[poz]
+        poz=poz-1
+    poz=poz+1    
+
+"""
 #afisare
 g = open('data.out','w')
 print (x)
 g.write(x)
 f.close
 g.close
+"""
+
+#afisare
+g = open('data.out','w')
+print (len(content_list))
+g.write(x)
+f.close
+g.close
+
 
 #COMPARA CU ALA DE JOS 
