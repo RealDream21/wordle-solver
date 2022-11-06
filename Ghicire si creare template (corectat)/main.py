802e4c6a6132b9f8a2f8dfbb2b1bb74bef8267d3
Rs0V <117230565+Rs0V@users.noreply.github.com>
2022-11-07 01:36:20 +0200

    Add files via upload

---
+++ Ghicire si creare template (corectat)/main.py
@@ -0,0 +1,30 @@
+import random
+
+file = open("wordlist.txt")
+lines = file.readlines()
+
+guess = random.choice(lines)
+guess = guess[:(len(guess) - 1)]
+myInput = input("Input word: ")
+
+feedback = "00000"
+guess_list = list(guess)
+feedback = list(feedback)
+for i in range(5):
+    if myInput[i] == guess[i]:
+        feedback[i] = guess[i]
+        guess_list[i] = "0"
+for i in range(5):
+    if feedback[i] == "0":
+        index = None
+        try:
+            index = guess_list.index(myInput[i])
+        except:
+            pass
+        else:
+            guess_list[index] = "0"
+            feedback[i] = "1"
+print()
+print(myInput)
+print(guess)
+print("".join(feedback))
