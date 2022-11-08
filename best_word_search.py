6aab23c4fdc7030e272bb4c5b0b333809a45c6d3
RealDream <fabianflorete@yahoo.com>
2022-11-08 09:30:28 +0200

    Merge cautarea celui mai bun cuvant
    De mers merge, dar imi calculeaza 1 cuv la 25 secunde :))

--- best_word_search.py
+++ best_word_search.py
@@ -27,13 +27,23 @@ def entropy_for_word(word):
     for template in B3_v2:
         nr_cuv_eliminate = 0
         for tryout_word in content_list:
-            elim = 0
+            word_freq = {}
+            start_letter = "A"
+            for index in range(26):
+                word_freq[start_letter] = [0,0]
+                int_start_letter = ord(start_letter) + 1
+                start_letter = chr(int_start_letter)
             for index in range(5):
-                if template[index] == "0" and word[index] not in tryout_word:
+                if template[index] == "0" and word[index] in tryout_word:
                     elim = 1
-                elif if template[index] == "2" and tryout_word[index] != word[index]:
+                elif template[index] == "2" and tryout_word[index] != word[index]:
                     elim = 1
-                elif template[index] == "1" and word[index] not in tryout_word:
+            for index in range(5):
+                if template[index] == 1:
+                    word_freq[word[index]][0] += 1
+                word_freq[tryout_word[index]][1] += 1
+            for index in range(5):
+                if template[index] == 1 and word_freq[word[index]][0] != word_freq[word[index]][1]:
                     elim = 1
             nr_cuv_eliminate += elim
             sum_entropy += entropy(nr_cuv_eliminate)
