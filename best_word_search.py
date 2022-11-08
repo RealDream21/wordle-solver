1e9dc362088098985d0433bc5e777f04224fe34b
RealDream <fabianflorete@yahoo.com>
2022-11-08 22:50:19 +0200

    Clarity changes

--- best_word_search.py
+++ best_word_search.py
@@ -30,11 +30,8 @@ def entropy_for_word(word):
         nr_cuv_eliminate = 0
         for tryout_word in content_list:
             elim = 0
-            #start_letter = "A"
             for index in range(26):
                 word_freq[alphabet[index]] = [0,0]
-                #int_start_letter = ord(start_letter) + 1
-                #start_letter = chr(int_start_letter)
             for index in range(5):
                 if template[index] == "0" and word[index] in tryout_word:
                     elim = 1
@@ -55,17 +52,15 @@ def entropy_for_word(word):
             nr_cuv_eliminate += elim
         sum_entropy += entropy(nr_cuv_eliminate)
     return sum_entropy
-best_word = "ACREI"
-maxim = entropy_for_word(best_word)
-k = 101
+best_word = "AERAI" #se introduce cel mai bun cuvant
+maxim = entropy_for_word(best_word) #nu trebuie schimbat nimic, calculeaza pt best_word
+k = 139 #ca si k se introcue fix ce este in [], deci nu trb sa calculezi nimic
+
 #Am verificat ACHIT, entropia actuala: 27.648036638297036. Pana acum cel mai bun cuvant este ABTIE,cu entropia 30.05835827752119 pozitia parcursa: [50]
-#Am verificat ACUMA, entropia actuala: 25.43801263215468. Pana acum cel mai bun cuvant este ACREI,cu entropia 31.743474933106608 pozitia parcursa: [83]
 #Am verificat ADAUS, entropia actuala: 25.647092420139366. Pana acum cel mai bun cuvant este ACREI,cu entropia 31.743474933106608 pozitia parcursa: [100]
 #Am verificat AERAJ, entropia actuala: 28.479690108407485. Pana acum cel mai bun cuvant este AERAI,cu entropia 31.993052025577068 pozitia parcursa: [139]
 #Am verificat AERAM, entropia actuala: 29.934945087431583. Pana acum cel mai bun cuvant este AERAI,cu entropia 31.993052025577068 pozitia parcursa: [140]
 #print(f"Am verificat {cuv}, entropia actuala: {entropie}. Pana acum cel mai bun cuvant este {best_word},cu entropia {maxim} pozitia parcursa: [{k}]")
-#fileR = open("save.out","r")
-#fileW = open("save.out", "a")
 
 for cuv in content_list[k:]:
     entropie = entropy_for_word(cuv)
@@ -75,7 +70,6 @@ for cuv in content_list[k:]:
         best_word = cuv
     print(f"Am verificat {cuv}, entropia actuala: {entropie}. Pana acum cel mai bun cuvant este {best_word},cu entropia {maxim} pozitia parcursa: [{k}]")
     k += 1
-#fileW.close()
-#fileR.close()
+
 print(cuv)
 print(entropie)
