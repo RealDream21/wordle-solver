af2f4a3e795113285c54f8879352d978b378349f
RealDream <fabianflorete@yahoo.com>
2022-11-08 09:46:05 +0200

    More tweaks la best_word_search.py
    still slow (and bugged, toate entropiile dau la fel)

--- best_word_search.py
+++ best_word_search.py
@@ -22,29 +22,35 @@ def entropy(nr_cuvinte_eliminate):
         return 0
     return shanon
 
+alphabet = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split()
+
 def entropy_for_word(word):
     sum_entropy = 0
     for template in B3_v2:
         nr_cuv_eliminate = 0
         for tryout_word in content_list:
             word_freq = {}
-            start_letter = "A"
+            #start_letter = "A"
             for index in range(26):
-                word_freq[start_letter] = [0,0]
-                int_start_letter = ord(start_letter) + 1
-                start_letter = chr(int_start_letter)
+                word_freq[alphabet[index]] = [0,0]
+                #int_start_letter = ord(start_letter) + 1
+                #start_letter = chr(int_start_letter)
             for index in range(5):
                 if template[index] == "0" and word[index] in tryout_word:
                     elim = 1
+                    break
                 elif template[index] == "2" and tryout_word[index] != word[index]:
                     elim = 1
+                    break
             for index in range(5):
                 if template[index] == 1:
                     word_freq[word[index]][0] += 1
-                word_freq[tryout_word[index]][1] += 1
+                if template[index] != 2:
+                    word_freq[tryout_word[index]][1] += 1
             for index in range(5):
                 if template[index] == 1 and word_freq[word[index]][0] != word_freq[word[index]][1]:
                     elim = 1
+                    break
             nr_cuv_eliminate += elim
             sum_entropy += entropy(nr_cuv_eliminate)
     return sum_entropy
@@ -57,7 +63,7 @@ for cuv in content_list:
     if entropie > maxim:
         maxim = entropie
         best_word = cuv
-    print(f"Am verificat {cuv}, pana acum cel mai bun cuvant este {best_word}, pozitia parcursa: {k}")
+    print(f"Am verificat {cuv}, pana acum cel mai bun cuvant este {best_word}, entropie: {entropie} pozitia parcursa: {k}")
     k += 1
 print(cuv)
 print(entropie)
