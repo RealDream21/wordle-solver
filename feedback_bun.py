e02421a240a6d726e5b37e9e82741a7be7c0f8ff
Radu Alexandru <rd_alexandruradu10@yahoo.ro>
2022-11-11 20:13:23 +0200

    feedback_bun

---
+++ feedback_bun.py
@@ -0,0 +1,28 @@
+import random
+
+#citire si crearea listei
+f = open('wordlist.txt','r')
+
+#luam un cuvant random
+x = random.choice(list(f))
+print(x)
+
+# cuvantul citit de la tastatura 
+cuv=input("cuv=")
+
+#lista initiata cu valoare vida
+s=" "
+
+#Program pentru ghcirea cuvantului
+for i in range (0,5): 
+    if cuv[i]==x[i]:
+        #print(2)
+        s+="2"+""
+    else:    
+        if cuv[i] in x:
+            #print(1)
+            s+="1"+""
+        else:
+            #print(0)
+           s+="0"+""
+print(s)
